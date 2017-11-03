01e45370e27370cfa1a85431732a220682a05761
Ingram Braun <me@ingram-braun.net>
2017-11-03 15:47:40 +0100

    adapted to biblatex 3.8 (dev)

--- odt.bat
+++ odt.bat
@@ -1,5 +1,6 @@
 call cleanup.bat
 mk4ht oolatex biblatex-archaeology_example "xhtml, charset=utf-8" -utf8
-biber biblatex-archaeology_example
+%USERPROFILE%\Documents\ingram\texmf\bin\biber.exe biblatex-archaeology_example
+::biber biblatex-archaeology_example
 mk4ht oolatex biblatex-archaeology_example "xhtml, charset=utf-8"  -utf8
 start biblatex-archaeology_example.odt
