428078875d86da56569af9083e2b8071cdbfb495
Ingram Braun <me@ingram-braun.net>
2017-11-15 18:38:49 +0100

    renamed .lbx files and biblatex version check added

--- example.bat
+++ example.bat
@@ -1,8 +1,8 @@
 REM compile the biblatex-archaeology example document
 call cleanup.bat
 lualatex -file-line-error biblatex-archaeology_example
-%USERPROFILE%\Documents\ingram\texmf\bin\biber.exe biblatex-archaeology_example
-::biber biblatex-archaeology_example 
+::%USERPROFILE%\Documents\ingram\texmf\bin\biber.exe biblatex-archaeology_example
+biber biblatex-archaeology_example 
 lualatex -file-line-error biblatex-archaeology_example
 lualatex -file-line-error biblatex-archaeology_example
 texworks biblatex-archaeology_example.pdf
