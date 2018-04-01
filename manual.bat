72993687179c3c3d3faa6eed2e65f7b7c933a467
Ingram Braun <me@ingram-braun.net>
2018-04-01 18:56:28 +0200

    attributed dates compatible with biblatex 3.11

--- manual.bat
+++ manual.bat
@@ -2,9 +2,12 @@ REM install the biblatex-archaeology package
 call bibtex.bat
 del biblatex-archaeology.pdf
 perl abbrev.pl
-perl abbrev.pl
+call cleanup.bat
 pdflatex -file-line-error biblatex-archaeology.dtx
+Biber biblatex-archaeology
 perl datamodel.pl
+call cleanup.bat
+pdflatex -file-line-error biblatex-archaeology.dtx
 Biber biblatex-archaeology
 makeindex -s gglo.ist -o biblatex-archaeology.gls biblatex-archaeology.glo
 makeindex -s gind.ist biblatex-archaeology.idx
