122460cb6cc56a0aba8627ab5f86f1fb4d7d1c9a
Ingram Braun <me@ingram-braun.net>
2018-05-19 19:10:15 +0200

    manual layout fixed

--- bibtex.bat
+++ bibtex.bat
@@ -1,6 +1,6 @@
 set INBIB=%USERPROFILE%/Documents/ingram/Texte/bib/ingram.bib
 call cleanup.bat
-pdflatex -file-line-error biblatex-archaeology.dtx
+lualatex -file-line-error biblatex-archaeology.dtx
 lualatex -file-line-error biblatex-archaeology_example
 perl bibextract.pl biblatex-archaeology biblatex-archaeology-nodoc.dtx manualBIB %INBIB%
 perl bibextract.pl biblatex-archaeology_example biblatex-archaeology.dtx exampleBIB %INBIB%
--- manual.bat
+++ manual.bat
@@ -4,16 +4,16 @@ call bibtex.bat
 del biblatex-archaeology.pdf
 perl abbrev.pl
 call cleanup.bat
-pdflatex -file-line-error biblatex-archaeology.dtx
+lualatex -file-line-error biblatex-archaeology.dtx
 Biber biblatex-archaeology
 perl datamodel.pl
 call cleanup.bat
-pdflatex -file-line-error biblatex-archaeology.dtx
+lualatex -file-line-error biblatex-archaeology.dtx
 Biber biblatex-archaeology
-pdflatex -file-line-error biblatex-archaeology.dtx
+lualatex -file-line-error biblatex-archaeology.dtx
 makeindex -s gglo.ist -o biblatex-archaeology.gls biblatex-archaeology.glo
 makeindex -s gind.ist biblatex-archaeology.idx
-pdflatex -file-line-error biblatex-archaeology.dtx
-pdflatex -file-line-error biblatex-archaeology.dtx
+lualatex -file-line-error biblatex-archaeology.dtx
+lualatex -file-line-error biblatex-archaeology.dtx
 texworks biblatex-archaeology.pdf
 pause>nul
