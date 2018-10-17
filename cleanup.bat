58bfa3f678fa46b52dacc28df4b527e8fb07fcc8
Ingram Braun <me@ingram-braun.net>
2018-10-17 16:43:35 +0200

    load xstring package

--- cleanup.bat
+++ cleanup.bat
@@ -2,7 +2,7 @@ REM Prepares the biblatex-archaeology development environment for a new run
  
 REM change shell colours and code page
 color 5F
-cp 65001
+chcp 65001
 
 REM remove intermediate and output files even from subfolders
 del /S *.4ct *.4tc *.aux *.bbl *.bcf *.blg *.css *.dvi *.glg *.glo *.gls *.hd *.html *.idv *.idx *.ilg *.ind *.lg *.lof *.log *.lot *.odt *.out *.run.xml *.tmp *.toc *.xdv *.xmpdata *.xmpi *.xref biblatex-archaeology_example.pdf
