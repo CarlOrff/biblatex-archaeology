e9962a1cd9c5ec1034860bdaffb30521de1a96e4
Ingram Braun <me@ingram-braun.net>
2018-10-26 13:50:11 +0200

    lualatex calls with -halt-on-error in batch files

--- manual.bat
+++ manual.bat
@@ -5,19 +5,19 @@ call bibtex.bat
 del biblatex-archaeology.pdf
 perl abbrev.pl
 call cleanup.bat
-lualatex -file-line-error biblatex-archaeology.dtx
+lualatex -file-line-error -halt-on-error biblatex-archaeology.dtx
 %USERPROFILE%\Documents\ingram\texmf\bin\biber.exe --trace biblatex-archaeology
 ::Biber biblatex-archaeology
 perl datamodel.pl
 call cleanup.bat
-lualatex -file-line-error biblatex-archaeology.dtx
+lualatex -file-line-error -halt-on-error biblatex-archaeology.dtx
 %USERPROFILE%\Documents\ingram\texmf\bin\biber.exe --trace biblatex-archaeology
 ::Biber biblatex-archaeology
-lualatex -file-line-error biblatex-archaeology.dtx
+lualatex -file-line-error -halt-on-error biblatex-archaeology.dtx
 makeindex -s gind.ist biblatex-archaeology.idx
-lualatex -file-line-error biblatex-archaeology.dtx
+lualatex -file-line-error -halt-on-error biblatex-archaeology.dtx
 makeindex -s gglo.ist -o biblatex-archaeology.gls biblatex-archaeology.glo
-lualatex -file-line-error biblatex-archaeology.dtx
-lualatex -file-line-error biblatex-archaeology.dtx
+lualatex -file-line-error -halt-on-error biblatex-archaeology.dtx
+lualatex -file-line-error -halt-on-error biblatex-archaeology.dtx
 texworks biblatex-archaeology.pdf
 pause>nul
--- nocite.bat
+++ nocite.bat
@@ -3,11 +3,11 @@ call cleanup.bat
 SET jobname=nocite
 SET document="\documentclass[12pt]{scrartcl}\usepackage{csquotes}\usepackage[style=mpkoeaw,]{biblatex}\IfFileExists{ingram-braun-local.sty}{\usepackage{ingram-braun-local}\addbibresource{\IBbibstrrgk}\addbibresource{\IBbibmain}}{\addbibresource{biblatex-examples.bib}}\begin{document}\nocite{*}\printbibliography\end{document}"
 
-lualatex -file-line-error -jobname=%jobname% %document%
+lualatex -file-line-error -halt-on-error -jobname=%jobname% %document%
 
 %USERPROFILE%\Documents\ingram\texmf\bin\biber.exe --trace %jobname%
 ::Biber %jobname%
 
-lualatex -file-line-error -jobname=%jobname% %document%
+lualatex -file-line-error -halt-on-error -jobname=%jobname% %document%
 
 texworks %jobname%.pdf
