7af1ce8f3603be51abce5b45992efd78c924417b
Ingram Braun <me@ingram-braun.net>
2019-11-03 16:01:25 +0100

    adapted to incompatibel change in biblatex 3.13

--- example.bat
+++ example.bat
@@ -2,8 +2,8 @@ REM compile the biblatex-archaeology example document
 chcp 65001
 call cleanup.bat
 lualatex -file-line-error -halt-on-error biblatex-archaeology_example
-%USERPROFILE%\Documents\ingram\texmf\bin\biber.exe --trace biblatex-archaeology_example
-::biber --trace biblatex-archaeology_example
+::%USERPROFILE%\Documents\ingram\texmf\bin\biber.exe --trace biblatex-archaeology_example
+biber --trace biblatex-archaeology_example
 lualatex -file-line-error -halt-on-error biblatex-archaeology_example
 lualatex -file-line-error -halt-on-error biblatex-archaeology_example
 texworks biblatex-archaeology_example.pdf
