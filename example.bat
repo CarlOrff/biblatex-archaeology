9157007bd71f3a426319d9afab3974b0f91ce81b
Ingram Braun <me@ingram-braun.net>
2019-12-25 12:02:05 +0100

    manual enhanced

--- example.bat
+++ example.bat
@@ -5,6 +5,6 @@ lualatex -file-line-error -halt-on-error biblatex-archaeology_example
 ::%USERPROFILE%\Documents\ingram\texmf\bin\biber.exe --trace biblatex-archaeology_example
 biber --trace biblatex-archaeology_example
 lualatex -file-line-error -halt-on-error biblatex-archaeology_example
-lualatex -file-line-error -halt-on-error biblatex-archaeology_example
+::lualatex -file-line-error -halt-on-error biblatex-archaeology_example
 texworks biblatex-archaeology_example.pdf
 pause>nul
