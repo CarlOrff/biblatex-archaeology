7ad01d4d1950821f3021d177049d586078e54cc3
Ingram Braun <me@ingram-braun.net>
2019-12-28 12:19:47 +0100

    MS Word conversion removed (*.odt is better)

--- odt.bat
+++ odt.bat
@@ -1,6 +1,2 @@
-call cleanup.bat
-mk4ht oolatex biblatex-archaeology_example "xhtml, charset=utf-8" -utf8
-%USERPROFILE%\Documents\ingram\texmf\bin\biber.exe biblatex-archaeology_example
-::biber biblatex-archaeology_example
-mk4ht oolatex biblatex-archaeology_example "xhtml, charset=utf-8"  -utf8
-start biblatex-archaeology_example.odt
+::make4ht -ux -f odt -c myconfig.cfg biblatex-archaeology_example.tex
+make4ht -ux -f odt biblatex-archaeology_example.tex
