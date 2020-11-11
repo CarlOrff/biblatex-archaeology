30bd09728c74174c6fb432914ce017f34f7d84d6
Ingram Braun <CarlOrff@users.noreply.github.com>
2020-11-11 14:35:51 +0100

    German intro draft added

--- example.bat
+++ example.bat
@@ -2,6 +2,7 @@ REM compile the biblatex-archaeology example document
 chcp 65001
 call cleanup.bat
 pdflatex -file-line-error -halt-on-error biblatex-archaeology_example
+::lualatex -file-line-error -halt-on-error biblatex-archaeology_example
 ::%USERPROFILE%\Documents\ingram\texmf\bin\biber.exe --trace biblatex-archaeology_example
 biber --trace biblatex-archaeology_example
 pdflatex -file-line-error -halt-on-error biblatex-archaeology_example
