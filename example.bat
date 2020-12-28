fdfb9104151daa0003b7ada785361f92e263bdef
Ingram Braun <CarlOrff@users.noreply.github.com>
2020-12-28 21:53:42 +0100

    zaak journals fixed

--- example.bat
+++ example.bat
@@ -3,8 +3,8 @@ chcp 65001
 call cleanup.bat
 ::pdflatex -file-line-error -halt-on-error biblatex-archaeology_example
 lualatex -file-line-error -halt-on-error biblatex-archaeology_example
-::%USERPROFILE%\Documents\ingram\texmf\bin\biber.exe --trace biblatex-archaeology_example
-biber --trace biblatex-archaeology_example
+%USERPROFILE%\Documents\ingram\texmf\bin\biber\biber.exe --trace biblatex-archaeology_example
+::biber --trace biblatex-archaeology_example
 ::pdflatex -file-line-error -halt-on-error biblatex-archaeology_example
 ::pdflatex -file-line-error -halt-on-error biblatex-archaeology_example
 lualatex -file-line-error -halt-on-error biblatex-archaeology_example
