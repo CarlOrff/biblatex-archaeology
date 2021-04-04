9a7401edbd664693c5573f4909130fb564ba7c77
Ingram Braun <CarlOrff@users.noreply.github.com>
2021-04-04 19:34:07 +0200

    switched off autopunct in some styles

---
+++ intro.bat
@@ -0,0 +1,17 @@
+chcp 65001
+call cleanup.bat
+
+SET biber=%USERPROFILE%\Documents\ingram\texmf\bin\biber\biber.exe
+SET biber=biber
+
+SET engine=xelatex
+SET engine=pdflatex
+SET engine=lualatex
+
+%engine% -file-line-error -halt-on-error biblatex-archaeology_intro_de
+%biber% --trace biblatex-archaeology_intro_de
+%engine% -file-line-error -halt-on-error biblatex-archaeology_intro_de
+makeindex -cg biblatex-archaeology_intro_de.idx
+%engine% -file-line-error -halt-on-error biblatex-archaeology_intro_de
+texworks biblatex-archaeology_intro_de.pdf
+pause>nul
