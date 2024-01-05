b882961ac825df7516dbf284794ff55754daef2a
Ingram Braun <CarlOrff@users.noreply.github.com>
2024-01-05 12:16:27 +0100

    manual one latex run more

--- manual.bat
+++ manual.bat
@@ -16,10 +16,13 @@ Biber biblatex-archaeology
 lualatex -file-line-error -halt-on-error biblatex-archaeology.dtx
 makeindex -s gind.ist biblatex-archaeology.idx
 lualatex -file-line-error -halt-on-error biblatex-archaeology.dtx
-makeindex -s gglo.ist -o biblatex-archaeology.gls -t biblatex-archaeology.glg biblatex-archaeology.glo
+makeindex -s gglo.ist -o biblatex-archaeology.gls biblatex-archaeology.glo
+::makeindex -s gglo.ist -o biblatex-archaeology.gls -t biblatex-archaeology.glg biblatex-archaeology.glo
 lualatex -file-line-error -halt-on-error biblatex-archaeology.dtx
 :: The doubling is neccessary to make glossaries consider its own length:
-makeindex -s gglo.ist -o biblatex-archaeology.gls -t biblatex-archaeology.glg biblatex-archaeology.glo
+makeindex -s gglo.ist -o biblatex-archaeology.gls biblatex-archaeology.glo
+::makeindex -s gglo.ist -o biblatex-archaeology.gls -t biblatex-archaeology.glg biblatex-archaeology.glo
+lualatex -file-line-error -halt-on-error biblatex-archaeology.dtx
 lualatex -file-line-error -halt-on-error biblatex-archaeology.dtx
 lualatex -file-line-error -halt-on-error biblatex-archaeology.dtx
 texworks biblatex-archaeology.pdf
